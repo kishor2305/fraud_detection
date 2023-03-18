2fe3af3eeb27a662260066fead527d6004c8a618
kishor2305 <kishoracharya91@gmail.com>
2023-03-19 01:18:33 +0530

    added all new models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,13 @@
 print("This is a code for ML model")
+
+def model1():
+    print("This is model 1")
+
+def model2():
+    print("This is model 2")
+
+def model3():
+    print("This is model 3")
+
+def model4():
+    print("This is model 4")
